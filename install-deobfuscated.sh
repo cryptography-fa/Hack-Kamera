747e0dcc0f28af8208ab6711ae31171c22672462
xNot_Found <41625152+hatakecnk@users.noreply.github.com>
2019-07-19 18:22:29 +0700

    Add files via upload

---
+++ install-deobfuscated.sh
@@ -0,0 +1,28 @@
+clear
+apt update -y
+apt upgrade -y
+apt install figlet -y
+apt install toilet -y
+apt install nano -y
+apt install ruby -y
+echo
+echo
+figlet -f pagga Basic installation | lolcat
+echo
+echo
+gem install lolcat
+apt install php -y
+apt install openssh -y
+apt install termux-api php
+clear
+echo
+echo
+echo
+echo -e " \e[1m\e[33m	 	Successful Installed Required Pkg..!!!"
+sleep 3
+clear
+echo
+echo
+echo -e "\e[36m		Now You Can Run \e[32m2. runnow\e[0m"
+echo
+bash runnow.sh
